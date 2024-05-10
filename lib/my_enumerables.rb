ad528f8f993790b5873224a8803fefb1645ae903
alex-assada <alex.assada@gmail.com>
2024-05-10 14:00:18 -0400

    #my_each & #my_each_with_index completed

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,32 @@ end
 # to this method
 class Array
   # Define my_each here
+
+
+  def my_each
+    self.each do |e|
+      yield(e)
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    self.each do |e|
+      yield(e, i)
+      i += 1
+    end
+  end
+
+
+  arr2 = []
+  arr = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+  arr.my_each_with_index { |e, i | puts "element = #{e * 2} and index = #{i * 2}" }
+
+
+
+
+  #arr.my_each { |n| puts n}
+  #arr.my_each_with_index { |e, i| puts "#{i}: #{e}" }
+
 end
+
